bd90754b41af06c4b2f3412f5343cffe2cbea644
Ad Soares <adnilson.soares@gmail.com>
2024-04-17 09:24:38 -0300

    fix compilacao babel

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = {
     '@babel/preset-typescript',
   ],
   plugins: [
+    ['@babel/plugin-transform-typescript', { allowDeclareFields: true } ],
     [
       'module-resolver',
       {
